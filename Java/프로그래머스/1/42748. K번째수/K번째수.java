be3e96167f809f7c7a4bb7564cc76f79f7c046e7
sowho.j <jangseohu112@gmail.com>
2026-09-23 19:46:34 +0900

    [level 1] Title: K번째수, Time: 3.13 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int a = 0; a < commands.length; a++) {
+            int i = commands[a][0];
+            int j = commands[a][1];
+            int k = commands[a][2];
+
+            int[] temp = Arrays.copyOfRange(array, i - 1, j);
+
+            Arrays.sort(temp);
+
+            answer[a] = temp[k - 1];
+        }
+
+        return answer;
+    }
+}
